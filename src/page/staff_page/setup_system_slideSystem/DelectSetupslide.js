12a995ab7808af115088e9cacb4939f198fcc40a
chaleomrat <70571942+chaleomrat@users.noreply.github.com>
2021-10-28 19:27:30 +0700

    Update DelectSetupslide.js

--- src/page/staff_page/setup_system_slideSystem/DelectSetupslide.js
+++ src/page/staff_page/setup_system_slideSystem/DelectSetupslide.js
@@ -17,7 +17,7 @@ const DelectSetupNameLogo = ({ slide }) => {
     const Delect = (e) => {
         e.preventDefault();
 
-        axios.delete("http://localhost:8080/Mback/public/Setup_slideDelect/" + slide.ssd_id)//ส่งค่าไปแอดใน DB
+        axios.delete("https://flashworkbackend.xyz/Mback/public/Setup_slideDelect/" + slide.ssd_id)//ส่งค่าไปแอดใน DB
             .then((res) => {
                 console.log(res.data.message);
                 if (res.data.message == "success") {
