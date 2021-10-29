80ccbd4e3b878dfa0535569b1dc8abaea67b8a38
chaleomrat <70571942+chaleomrat@users.noreply.github.com>
2021-10-29 15:41:01 +0700

    Update scholarship_box.js

--- src/component/scholarship_box.js
+++ src/component/scholarship_box.js
@@ -4,31 +4,31 @@ import { Card, Button, CardBody, CardHeader } from "reactstrap";
 
 const Scholarship_Box = ( {id} ) => {
    
-    const [Mscholars, setMscholar] = useState([]);
+    const [Mainscholars, setMscholars] = useState([]);
     useEffect(() => {
         axios.get("http://flashworkbackend.xyz/Mback/public/findMshcholarship/" + id)
             .then((response) => {
-                setMscholar(response.data);
+                setMscholars(response.data);
             });
     }, [id]);
       return (
         <>
                 <div style={{  }}>
-                    <img src={Mscholars.m_img} alt="" style={{ borderTopLeftRadius: '10px', borderTopRightRadius: '10px', width: '100%', marginBottom: '-10px' }}/>
+                    <img src={Mainscholars.m_img} alt="" style={{ borderTopLeftRadius: '10px', borderTopRightRadius: '10px', width: '100%', marginBottom: '-10px' }}/>
                 </div>
                 <Card className="CardBackground-2" style={{ minHeight: '200px' }}>
-                    <a href={"/scholarshipMain/" + Mscholars.id}>
+                    <a href={"/scholarshipMain/" + Mainscholars.id}>
                         <CardHeader className="" style={{ background: "#dadce0", borderRadius: "10px", borderBottom: "0px" }}>
                             <h6 className="text-dark" style={{ margin: '0px' }}>
-                                <b>{Mscholars.name} ปี {Mscholars.year}</b>
+                                <b>{Mainscholars.name} ปี {Mainscholars.year}</b>
                             </h6>
                         </CardHeader>
                     </a>
                     <CardBody className="text-secondary" style={{ padding: "10px" }}>
                     <div className="borderline" />
-                       {Mscholars.content}{id}
+                       {Mainscholars.content}
                         <div className="borderline" />
-                        <Button href={"/scholarshipMain/" + Mscholars.id} className="Button-Style" outline >อ่านทั้งหมด</Button>
+                        <Button href={"/scholarshipMain/" + Mainscholars.id} className="Button-Style" outline >อ่านทั้งหมด</Button>
                     </CardBody>
                 </Card>
 
