dc65d376cbb78190eb780f74c88cbdff7a8ebe5a
chaleomrat <70571942+chaleomrat@users.noreply.github.com>
2021-10-29 15:10:58 +0700

    Update home-all.js

--- src/page/home-all.js
+++ src/page/home-all.js
@@ -75,9 +75,9 @@ const Home = (props) => {
                                 <div className="EdgeRow-1">
                                     <Row>
                                         {Mscholar.map((scholar) => {
-
+                                            
                                             return (
-                                                <div className="col-12 col-sm-6 col-lg-6 col-BoxContentSetting">{scholar.msch_id}
+                                                <div className="col-12 col-sm-6 col-lg-6 col-BoxContentSetting">
                                                     <div key={scholar.msch_id}><Scholarship_Box id={scholar.msch_id} /></div>
                                                 </div>
                                             );
