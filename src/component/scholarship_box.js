aa289d518d484c9f90213860202fbb01b79e4844
chaleomrat <70571942+chaleomrat@users.noreply.github.com>
2021-10-29 16:21:26 +0700

    Update scholarship_box.js

--- src/component/scholarship_box.js
+++ src/component/scholarship_box.js
@@ -11,36 +11,30 @@ const Scholarship_Box = ({ id }) => {
                 setMscholar(response.data);
             });
     }, [id]);
-      
-         {Mscholar.map((scholar) => {
-                                            
-                                            return (
-                                                <>
-                                                   <div style={{  }}>
-                                                <img src={scholar.m_img} alt="" style={{ borderTopLeftRadius: '10px', borderTopRightRadius: '10px', width: '100%', marginBottom: '-10px' }}/>
-                                            </div>
-                                            <Card className="CardBackground-2" style={{ minHeight: '200px' }}>
-                                                <a href={"/scholarshipMain/" + scholar.id}>
-                                                    <CardHeader className="" style={{ background: "#dadce0", borderRadius: "10px", borderBottom: "0px" }}>
-                                                        <h6 className="text-dark" style={{ margin: '0px' }}>
-                                                            <b>{scholar.name} ปี {scholar.year}</b>
-                                                        </h6>
-                                                    </CardHeader>
-                                                </a>
-                                                <CardBody className="text-secondary" style={{ padding: "10px" }}>
-                                                <div className="borderline" />
-                                                   {scholar.content}
-                                                    <div className="borderline" />
-                                                    <Button href={"/scholarshipMain/" + scholar.id} className="Button-Style" outline >อ่านทั้งหมด</Button>
-                                                </CardBody>
-                                            </Card> </>
-                                             
-                                            );
-                                        })}
-               
+      return (
+        <>
+                <div style={{  }}>
+                    <img src={Mscholar.m_img} alt="" style={{ borderTopLeftRadius: '10px', borderTopRightRadius: '10px', width: '100%', marginBottom: '-10px' }}/>
+                </div>
+                <Card className="CardBackground-2" style={{ minHeight: '200px' }}>
+                    <a href={"/scholarshipMain/" + Mscholar.id}>
+                        <CardHeader className="" style={{ background: "#dadce0", borderRadius: "10px", borderBottom: "0px" }}>
+                            <h6 className="text-dark" style={{ margin: '0px' }}>
+                                <b>{Mscholar.name} ปี {Mscholar.year}</b>
+                            </h6>
+                        </CardHeader>
+                    </a>
+                    <CardBody className="text-secondary" style={{ padding: "10px" }}>
+                    <div className="borderline" />
+                       {Mscholar.content}
+                        <div className="borderline" />
+                        <Button href={"/scholarshipMain/" + Mscholar.id} className="Button-Style" outline >อ่านทั้งหมด</Button>
+                    </CardBody>
+                </Card>
 
               
-     
+        </>
+    )
 }
 
 export default Scholarship_Box;
